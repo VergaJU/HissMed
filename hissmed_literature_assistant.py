a83c04743eb871b0a7138e3b41a1525b81b75ad2
VergaJU <jacopoumberto.verga@gmail.com>
2025-03-04 12:03:51 +0000

    dumb arguments error resolved

--- hissmed_literature_assistant.py
+++ hissmed_literature_assistant.py
@@ -16,7 +16,7 @@ def hisscat_download_literature(user_message, cat):
     settings = cat.mad_hatter.get_plugin().load_settings()
     log.info(f"User email: {settings['email']}")  # Log the user email
     email = settings['email']
-    top_n_articles = settings['top_n_articles']
+    n_top_articles = settings['top_n_articles']
     top_references = settings['top_references'] # not used now
     citation_weight = settings['citation_weight']
     year_weight = settings['year_weight']
@@ -24,7 +24,7 @@ def hisscat_download_literature(user_message, cat):
     PapersDownloader.set_email(email=email)
     log.info(f"Saving articles in {os.path.join(os.getcwd(),'literature')}")  # Log the output folder
     PapersDownloader.create_directory()
-    PapersDownloader.batch_download_pdfs(query=user_message, top_n_articles=top_n_articles, citation_w=citation_weight, year_w=year_weight, journal_w=journal_weight)
+    PapersDownloader.batch_download_pdfs(query=user_message, n_top_articles=n_top_articles, citation_w=citation_weight, year_w=year_weight, journal_w=journal_weight)
     log.info(f"Downloaded articles") # Log the download status
     return user_message
 
